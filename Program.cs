734e0d7641259d1df873e7ba1ca80201ad5d6e77
Evgeniy <e.vasiliev.86@mail.ru>
2022-08-05 13:38:17 +0300

    Creat Program

--- Program.cs
+++ Program.cs
@@ -1,6 +1,36 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует
-// массив из строк, длина которых меньше либо равна 3 символа. 
+// массив из строк, длина которых меньше либо равна 3 символа.
 // Первоначальный массив можно ввести с клавиатуры, либо залить на старте
 // выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
 // лучше обойтись исключительно массивами.
+
+string[] array1 = new string[7] { "Hello", "2", "world", ":-)", "ok", "yes", "Home" };
+string[] array2 = new string[array1.Length];
+
+void Print(string[] array)
+{
+    System.Console.Write("[");
+    System.Console.Write(array[0]);
+    for (int i = 1; i < array.Length; i++)
+    {
+        System.Console.Write(", " + array[i]);
+    }
+    System.Console.WriteLine("]");
+}
+
+void arrayThreeSymbols(string[] array1, string[] array2)
+{
+    int j = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[j] = array1[i];
+            j++;
+        }
+    }
+}
+
+arrayThreeSymbols(array1, array2);
+Print(array2.Where(x => x != null).ToArray());
 
